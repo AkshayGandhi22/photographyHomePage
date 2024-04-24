3f0ac8be3ae20dc032c3c2ff6224c49d162fb7fe
Akshay Gandhi <akshay@zurumedia.com>
2024-04-24 15:23:54 +0530

    home page is done

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+// MENU TOGGLE BUTTON
+$(document).ready(function () {
+    $('#nav-icon1').click(function () {
+        $(this).toggleClass('open');
+    });
+
+    $('#nav-icon1').click(function () {
+        $("body").toggleClass('bodyover');
+    });
+});
--- js/slider.js
+++ js/slider.js
@@ -1,27 +1,88 @@
-var owl = $('.owl-carousel');
-owl.owlCarousel({
-  nav: true,
-  loop: false,
-  dots:false,
-  responsive: {
-    1000: {
-      items: 1
+$(function() {
+  // Initialize the testimonial carousel
+  var testimonialCarousel = $('.testimonialCarousel');
+  testimonialCarousel.owlCarousel({
+    items: 1,
+    loop: false,
+    nav: true,
+    dots: false,
+    onInitialized: counter,
+    onChanged: counter
+  });
+
+  // Counter function for the testimonial carousel
+  function counter(event) {
+    var element = event.target;
+    var items = event.item.count;
+    var item = event.item.index + 1;
+
+    if (item > items) {
+      item = item - items;
     }
+    $('#testimonialCounter').html(item + " / " + items);
   }
-})
+});
 
-// Number Count
-  var totalItems = $('.slide').length;
-var currentIndex = $('.active').index() + 1;
-$('.num').html(''+currentIndex+'/'+totalItems+'');
-var  owl = $('.owl-carousel');
-owl.owlCarousel();
-$('.owl-prev').click(function() {
-    currentIndex = $('.owl-item.active').index() + 1;
-   $('.num').html(''+currentIndex+'/'+totalItems+'');
-})
+$(function() {
+  // Initialize the banner slider carousel
+  var bannerCarousel = $('.owl-carousel');
+  bannerCarousel.owlCarousel({
+    items: 1,
+    loop: false,
+    nav: true,
+    dots: false,
+    onInitialized: counter,
+    onChanged: counter
+  });
 
-$('.owl-next').click(function() { 
-    currentIndex = $('.owl-item.active').index() + 1;
-   $('.num').html(''+currentIndex+'/'+totalItems+'');
-})
+  // Counter function for the banner slider carousel
+  function counter(event) {
+    var element = event.target;
+    var items = event.item.count;
+    var item = event.item.index + 1;
+
+    if (item > items) {
+      item = item - items;
+    }
+    $('#counter').html(item + " / " + items);
+  }
+});
+
+$(function() {
+  // Initialize the banner slider carousel
+  var bannerCarousel = $('.imgSlider');
+  
+  bannerCarousel.owlCarousel({
+    margin: 40,
+    items: 3,
+    loop: false,
+    nav: true,
+    dots: false,
+    onInitialized: counter,
+    onChanged: counter,
+    responsive: {
+      0: {
+        items: 1 
+      },
+      600: {
+        items: 2 
+      },
+      1000: {
+        items: 3
+      }
+    }
+  });
+
+  // Counter function for the banner slider carousel
+  function counter(event) {
+    var element = event.target;
+    var items = event.item.count;
+    var item = event.item.index + 1;
+
+    if (item > items) {
+      item = item - items;
+    }
+    $('#imgcounter').html(item + " / " + items);
+  }
+});
+
